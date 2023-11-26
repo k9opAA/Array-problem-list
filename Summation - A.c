6b6023865de69dc96d4c142309d7450b87a924e4
k9opAA <140571734+k9opAA@users.noreply.github.com>
2023-11-26 19:27:06 +0600

    Create Summation - A.c

---
+++ Summation - A.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main() {
+    int n, sum;
+    scanf("%d", &n);
+
+    int a[n];
+    for (int i = 0; i < n; i++) {
+        scanf("%d", &a[i]);
+    }
+    for (int i = 0; i < n; i++) {
+        sum+=a[i];
+    }
+    printf("%d", sum);
+
+    return 0;
+}
