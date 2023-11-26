5f402b23cc867ea6ca9efe23e1162ac64483d368
k9opAA <140571734+k9opAA@users.noreply.github.com>
2023-11-26 19:24:44 +0600

    Update 5.c

--- 5.c
+++ 5.c
@@ -2,20 +2,16 @@
 
 int main() {
     int n;
-
-    printf("Enter the number of elements in the array: ");
     scanf("%d", &n);
 
     int a[n];
 
-    printf("Enter %d elements of the array:\n", n);
     for (int i = 0; i < n; i++) {
         scanf("%d", &a[i]);
     }
 
     int s = n / 2;
 
-    printf("Last half of the array:\n");
     for (int i = s; i < n; i++) {
         printf("%d\n", a[i]);
     }
