bc0e4a69ce68ee17c9eb8fb2added8d003b7c48e
k9opAA <140571734+k9opAA@users.noreply.github.com>
2023-11-26 19:33:36 +0600

    Update Summation - A.c

--- Summation - A.c
+++ Summation - A.c
@@ -1,17 +1,21 @@
-#include <stdio.h>
-
-int main() {
-    int n, sum;
+#include<stdio.h>
+#include<stdlib.h>
+int main()
+{
+    int n, i, sum;
     scanf("%d", &n);
-
     int a[n];
-    for (int i = 0; i < n; i++) {
-        scanf("%d", &a[i]);
+    for(i=0;i<n;i++)
+    {
+        scanf("%lld", &a[i]);
     }
-    for (int i = 0; i < n; i++) {
+    for(i=0,sum=0; i<n;i++)
+    {
         sum+=a[i];
     }
-    printf("%d", sum);
+    sum=abs(sum);
+    printf("%lld\n", sum);
+
 
     return 0;
 }
