e584c568b6ed30f44d56c4f38249b14354507cc1
k9opAA <140571734+k9opAA@users.noreply.github.com>
2023-11-26 20:58:03 +0600

    Create Fibonacci - O.c

---
+++ Fibonacci - O.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main() {
+    int N;
+    scanf("%d", &N); // Read the value of N
+
+    int fib[N + 1];
+    fib[1] = 0;
+    fib[2] = 1;
+
+    for (int i = 3; i <= N; i++) {
+        fib[i] = fib[i - 1] + fib[i - 2];
+    }
+
+    printf("%d\n", fib[N]);
+
+    return 0;
+}
